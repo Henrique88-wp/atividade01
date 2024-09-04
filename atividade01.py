ef3758c3c129f72464276a2ad21fdc4d26ec40c0
Henrique88-wp <phdsn411@gmail.com>
2024-09-04 14:47:05 -0300

    atividade 01 que não ta rodando, teste

---
+++ atividade01.py
@@ -0,0 +1,6 @@
+# Crie um programa que receba dois números do usuário e exiba a soma desses números.
+
+n1 = int(input("digite um valor"))
+n2 = int(input("digite outro valor"))
+soma = n1 + n2
+print(f"a soma é {soma}")
